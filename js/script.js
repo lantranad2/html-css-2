562b6ff4f034f64e5dd60ad245e7a30a0e18604a
Tran Lan <tranthanhlan.ad2@gmail.com>
2021-09-12 17:12:49 +0700

    Create a carousel component

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const icon = document.querySelector(".accordion__icon");
 const accordion = document.querySelector(".accordion");
+const dots = document.querySelector(".carousel__dots");
 // console.log(icon);
 
 icon.addEventListener("click", function (e) {
   accordion.classList.toggle("open");
+});
+
+dots.addEventListener("click", function (e) {
+  console.log(e.target);
 });
